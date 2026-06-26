a2a7aa8bf03537a64aa34c3858fe72cca57e93d7
brayooooooook <brianayiecha52@gmail.com>
2026-06-26 17:49:31 +0300

    Add Firebase configuration and initialization

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyYnHia2us8Ow2HfywH10lQQD4mk9AqH0",
+  authDomain: "arcane-monolith-bgbcx.firebaseapp.com",
+  projectId: "arcane-monolith-bgbcx",
+  storageBucket: "arcane-monolith-bgbcx.firebasestorage.app",
+  messagingSenderId: "824929257592",
+  appId: "1:824929257592:web:ee6d9ade425adf54e7058d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

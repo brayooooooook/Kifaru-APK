f84da6e50ceda2d9446dcef75c19b1351ae4c9a9
brayooooooook <brianayiecha52@gmail.com>
2026-06-26 19:38:07 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -26,17 +26,16 @@ export interface SubjectInfo {
   name: string;
 }
 
-export const SUBJECTS: SubjectInfo[] = [
   { code: "ENG", name: "English Language" },
   { code: "KIS", name: "Kiswahili" },
   { code: "MAT", name: "Mathematics" },
   { code: "SCI", name: "Integrated Science" },
   { code: "PTS", name: "Pre-Technical Studies" },
   { code: "CAS", name: "Creative Arts and Sports" },
+  { code: "AGR", name: "Agriculture" },
   { code: "SST", name: "Social Studies" },
-  { code: "CRE", name: "Christian Religious Education" },
-  { code: "AGR", name: "Agriculture" }
-];
+  { code: "CRE", name: "Christian Religious Education" }
+
 
 export interface CBEBandDetails {
   band: "EE1" | "EE2" | "ME1" | "ME2" | "AE1" | "AE2" | "BE1" | "BE2";

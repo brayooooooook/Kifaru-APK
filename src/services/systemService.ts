e6203c65fd979e0ab49320020ec7a95bfe0b3331
brayooooooook <brianayiecha52@gmail.com>
2026-06-27 09:40:17 +0300

    Add function to retrieve system data from Firestore

--- src/services/systemService.ts
+++ src/services/systemService.ts
@@ -1,1 +1,14 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
+export async function getSystemData() {
+  const ref = doc(db, "system", "main");
+
+  const snap = await getDoc(ref);
+
+  if (snap.exists()) {
+    return snap.data();
+  }
+
+  return null;
+}
